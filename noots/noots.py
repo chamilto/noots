995967c0bd300edc2a48fb7008a54a3bafddaccd
Corey Hamilton <hamiltoncw1993@gmail.com>
2018-01-07 14:48:47 -0500

    small changes. renaming things.

--- noots/noots.py
+++ noots/noots.py
@@ -13,6 +13,15 @@ config_file_path = os.path.join(home, '.noots.ini')
 conf = configparser.ConfigParser()
 conf.read(config_file_path)
 NOOTS_PATH = conf['NOOTS']['note_path']
+LOGO  = """
+    _   __            __
+   / | / /___  ____  / /______
+  /  |/ / __ \/ __ \/ __/ ___/
+ / /|  / /_/ / /_/ / /_(__  )
+/_/ |_/\____/\____/\__/____/
+        """
+
+NOTE_FILE_EXT = '.noot'
 
 
 def path_leaf(path):
@@ -54,12 +63,12 @@ class SearchManager(object):
 
     def refresh_fn_cache(self):
         """Update filename cache using glob pattern."""
-        glob_pat = os.path.join(NOOTS_PATH, '*.noot')
+        glob_pat = os.path.join(NOOTS_PATH, '*{0}'.format(NOTE_FILE_EXT))
         file_list = glob.glob(glob_pat)
-        self._fn_cache = [path_leaf(f).replace('.noot', '') for f in file_list]
+        self._fn_cache = [path_leaf(f).replace('{0}'.format(NOTE_FILE_EXT), '') for f in file_list]
 
     def read_from_match(self):
-        read_path = os.path.join(NOOTS_PATH, self.matched_title + '.noot')
+        read_path = os.path.join(NOOTS_PATH, self.matched_title + NOTE_FILE_EXT)
         try:
             with open(read_path) as fin:
                 data = fin.read()
@@ -69,16 +78,15 @@ class SearchManager(object):
             pass
 
 
-class CLI(object):
-    """Main CLI Object. Uses one method, input_callback, for all input handling.
-       Input handling can move to individual widgets if this becomes burdensome.
-    """
+class AppController(object):
+    """Main Application Controller. Uses one method, handle_input, for all input handling."""
+
     def __init__(self):
         self.suggestion_content = []
         self.main_lw = None
         self.main_suggestion_listbox = None
-        self.suggestions_listbox = None
-        self.header_text = ''
+        self.suggestions_listbox_container = None
+        self.help_text = ''
         self.header = None
         self.header_div = None
         self.header_pile = None
@@ -90,21 +98,20 @@ class CLI(object):
         self.main_box = None
         self.main_cols = None
 
-        self.init_header()
-        self.init_search_bar()
-        self.init_suggestion_list_box()
-        self.init_body()
-        self.init_main_container()
+        self._init_header()
+        self._init_search_bar()
+        self._init_suggestion_list_box()
+        self._init_body()
+        self._init_main_container()
 
         self.search_manager = SearchManager()
         self.search_chars = []
 
+        # Fill note suggestion listbox with all notes on startup
         self.search_manager.populate_sorted_filenames_from_fn_cache()
-        self.update_suggestion_list()
+        self._update_suggestion_list()
 
-        self.set_body('')
-
-    def init_main_container(self):
+    def _init_main_container(self):
         footer_text = ('foot', [
             "Noots  ",
             ('key', "|  (?) "), "help menu ",
@@ -116,72 +123,61 @@ class CLI(object):
             footer=self.footer)
 
         self.main_box = urwid.LineBox(self.main_frame)
-        self.main_cols = urwid.Columns([('weight', 2, self.suggestions_listbox), ('weight', 5, self.main_box),])
+        self.main_cols = urwid.Columns([('weight', 2, self.suggestions_listbox_container), ('weight', 5, self.main_box),])
         self.main_cols.set_focus(0)
 
-    def init_body(self):
+    def _init_body(self):
         self.body_edit_text = urwid.Edit('', multiline=True)
         self.body = urwid.Filler(self.body_edit_text, 'top')
 
-    def init_search_bar(self):
+    def _init_search_bar(self):
         self.search_level_text = urwid.Text('Search: ')
         self.search_box = urwid.LineBox(self.search_level_text)
 
-    def init_header(self):
-        logo  = """
-    _   __            __
-   / | / /___  ____  / /______
-  /  |/ / __ \/ __ \/ __/ ___/
- / /|  / /_/ / /_/ / /_(__  )
-/_/ |_/\____/\____/\__/____/
-        """
-        self.header_text = ("{logo}\n"
+    def _init_header(self):
+        self.help_text = ("{logo}\n"
                             "Ctrl-D anytime to save current note. \n"
                             "Ctrl-P to focus search/title bar.\n"
                             "Press Ctrl-E to focus note editor\n"
-                            "Hold alt to copy text.\n".format(logo=logo))
-        self.header = urwid.Text(self.header_text)
+                            "Hold alt to copy text.\n".format(logo=LOGO))
+        self.header = urwid.Text(self.help_text)
         self.header_div = urwid.Divider('.')
         self.header_pile = urwid.Pile([self.header, self.header_div])
 
-    def init_suggestion_list_box(self):
+    def _init_suggestion_list_box(self):
         self.suggestion_content = [self.search_box, urwid.Divider()]
 
         self.main_lw = urwid.SimpleFocusListWalker(self.suggestion_content)
         self.main_suggestion_listbox = urwid.ListBox(self.main_lw)
 
-        suggestion_items = urwid.Padding(self.main_suggestion_listbox, left=2, right=2)
-        self.suggestions_listbox = urwid.Overlay(suggestion_items, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
-        align='center', width=('relative', 100),
-        valign='middle', height=('relative', 100),
-        min_width=20, min_height=9)
+        self.suggestions_listbox_container = urwid.Padding(self.main_suggestion_listbox, left=2, right=2)
 
 
-    def show_help(self):
-        self.header.set_text(self.header_text)
+    def _show_help(self):
+        self.header.set_text(self.help_text)
 
-    def update_suggestion_list(self, clean=False):
+    def _update_suggestion_list(self, clean=False):
         suggestion_content = [self.search_box, urwid.Divider()]
 
         if not clean:
             for label in self.search_manager.sorted_filenames:
                 b = urwid.Button(label)
-                urwid.connect_signal(b, 'click', self.on_list_item_clicked, label)
+                urwid.connect_signal(b, 'click', self._on_list_item_clicked, label)
                 suggestion_content.append(urwid.AttrMap(b, None, focus_map='reversed'))
 
         self.main_lw[:] = urwid.SimpleFocusListWalker(suggestion_content)
 
-    def on_list_item_clicked(self, button, label):
+    def _on_list_item_clicked(self, button, label):
         self.update(search_string=label, update_list=False)
 
-    def save_note(self):
+    def _save_note(self):
         if self.search_manager.matched_title:
             title = self.search_manager.matched_title
         else:
             title = ''.join(self.search_chars)
 
         title = title.replace(' ', '_')
-        title_fn = title + '.noot'
+        title_fn = title + NOTE_FILE_EXT
 
         filepath = os.path.join(NOOTS_PATH, title_fn)
 
@@ -189,22 +185,29 @@ class CLI(object):
             fout.write(self.body_edit_text.get_edit_text())
 
         self.search_manager.refresh_fn_cache()
-        self.set_header('Saved!')
         self.update(title)
 
-    def set_body(self, text):
+    def _set_body(self, text):
         self.body_edit_text.set_edit_text(text)
 
-    def set_header(self, text):
+    def _set_header(self, text):
         self.header.set_text(text)
 
-    def input_callback(self, key):
+    def _set_search_text(self, text):
+        self.search_level_text.set_text(text)
+
+    def handle_input(self, key):
+        """Any keypress that is not handled by the Listwalker (Up, Down)
+           or the Edit widget will bubble up to this method. It would probably be
+           better to have the individual widgets implement their own keypress, but
+           with such a simple UI, this should be fine.
+        """
         if key == '?':
-            self.show_help()
+            self._show_help()
             return
 
         if key == 'ctrl d':
-            self.save_note()
+            self._save_note()
             return
 
         if key in  ('up', 'ctrl e'):
@@ -219,8 +222,9 @@ class CLI(object):
         if key == 'esc':
             self.search_chars[:] = []
             self.update()
-            self.set_body('')
-            self.set_header('')
+            self._set_body('')
+            self._set_header('')
+            self.main_cols.set_focus(0)
             return
 
         if key == 'backspace':
@@ -231,7 +235,6 @@ class CLI(object):
         elif key not in ('enter', 'meta', 'down', 'up', 'right', 'left') and type(key) == str:
             self.search_chars.append(key)
 
-
         try:
             self.update()
         except:
@@ -239,39 +242,38 @@ class CLI(object):
 
     def update(self, search_string='', update_list=True):
         if not self.search_chars:
-            self.body_edit_text.set_edit_text('')
+            self._set_body('')
 
         search_string = search_string or ''.join(self.search_chars).strip()
         display_txt = "Search:  {0}".format(search_string)
-        self.search_level_text.set_text(display_txt)
+        self._set_search_text(display_txt)
 
-        self.search_and_set_body_and_header(search_string, update_list)
-
-
-    def search_and_set_body_and_header(self, search_string, update_list):
         self.search_manager.search(search_string)
 
         if update_list:
-            self.update_suggestion_list()
+            self._update_suggestion_list()
 
         if self.search_manager.matched_title:
             try:
-                self.set_body(self.search_manager.read_from_match())
+                self._set_body(self.search_manager.read_from_match())
             except AttributeError:
                 pass
-            self.set_header(self.search_manager.matched_title)
+
+            self._set_header(self.search_manager.matched_title)
         else:
             if search_string:
-                self.set_header('(New): {0}.noot'.format(search_string.replace(' ', '_')))
-            else:
-                self.set_header('Welcome to Noots!')
+                self._set_header('(New): {0}{1}'.format(
+                    search_string.replace(' ', '_'),
+                    NOTE_FILE_EXT)
+                )
 
-            self.set_body('')
-
+            self._set_body('')
 
     def main(self):
-        self.loop = urwid.MainLoop(self.main_cols, unhandled_input=self.input_callback)
+        self.loop = urwid.MainLoop(self.main_cols, unhandled_input=self.handle_input)
         self.loop.run()
+
+
 if __name__ == '__main__':
-    c = CLI()
-    c.main()
+    controller = AppController()
+    controller.main()
